538f278bd060e7228faca96f31cd78385e84d60d
RahulKrSharma2004 <kumarrahulbasdiha@gmail.com>
2023-08-30 13:36:53 +0530

    added js code

---
+++ Introduction_to_js_problem.js
@@ -0,0 +1,42 @@
+console.log("Masai School");
+console.log("A Transformation in Education") 
+
+
+
+let name = "NAME - RAHUL KUMAR"
+console.log(Name);
+fatherName = "FATHER'S NAME - BIJAY KUMAR SHARMA"
+console.log(fatherName);
+motherName = "MOTHER'S NAME - SHANTI DEVI"
+console.log(motherName)
+
+
+
+
+var Name = "RAHUL KUMAR"
+let age = 19
+console.log(Name,age);
+
+
+
+
+var Name = " Name - Rahul Kumar"
+let school = " School - A.N.College Patna"
+let grade = " Grade - 1st"
+let section = " Section - A"
+let rollNo = " Roll no - 218"
+let education = " Education - Graduation"
+let space = "                                       "
+let marks = " Math-85,Physics-80,English-82 "
+console.log("👉~~~~~~~~ *𝐑𝐄𝐒𝐔𝐋𝐓𝐒* ~~~~~~~~👈")
+console.log(name)
+console.log(school)
+console.log(grade)
+console.log(section)
+console.log(rollNo)
+console.log(education)
+console.log(space)
+console.log("^^^^^^^^^^^^^ M̳A̳R̳K̳S̳ O̳B̳T̳A̳I̳N̳E̳D̳ ^^^^^^^^^^^^^^^")
+console.log(marks)
+
+
